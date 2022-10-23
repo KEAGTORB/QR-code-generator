cf4575c421db8ab36709b68272e92672c620241c
MINHAJ <92469140+KEAGTORB@users.noreply.github.com>
2022-10-23 10:18:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ qr = qrcode.QRCode(
 qr.add_data(input_URL)
 qr.make(fit=True)
 
-img = qr.make_image(fill_color="red", back_color="white")
+img = qr.make_image(fill_color="black", back_color="white")
 img.save("url_qrcode.png")
 
 print(qr.data_list)
